0dc6fa1662504fa7f112e2ebebb109ddcd9904f7
ardisrtilson <email@example.com>
2020-08-28 13:03:16 -0500

    Made other people s news uneditable

--- src/scripts/home/HomeHTMLGenerator.js
+++ src/scripts/home/HomeHTMLGenerator.js
@@ -19,8 +19,6 @@ if (type === "theirs"){
         <div class="news--synopsis">Synopsis: ${ newsObject.synopsis }</div>
         <div class="news--URL"><a href="${ newsObject.url }">View Article</a></div>
         <div class="news--date">Date: ${ newsObject.date }</div>
-        <button id="deleteNews--${newsObject.id}">Delete Article</button>
-        <button id="editNews--${newsObject.id}">Edit Article</button>
         </section>
         <article>
             `

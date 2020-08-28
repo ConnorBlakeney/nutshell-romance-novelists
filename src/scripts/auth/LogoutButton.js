efffd2b3a3caa01ac679ed5daaebf59e2383e095
Raparker88 <raparker88@gmail.com>
2020-08-28 11:19:18 -0500

    added some comments

--- src/scripts/auth/LogoutButton.js
+++ src/scripts/auth/LogoutButton.js
@@ -1,4 +1,6 @@
-
+//author: Rebecca Parker
+//purpose: renders logout button and handles click event to clear session storage and 
+//reload page on click
 
 const contentTarget = document.querySelector(".logout")
 const eventHub = document.querySelector(".container")
@@ -11,6 +13,7 @@ export const logout = () => {
 eventHub.addEventListener("click", event => {
     if(event.target.id === "logoutButton"){
         sessionStorage.clear()
+        //reloads browser as if refresh was clicked
         location.reload()
     }
 })

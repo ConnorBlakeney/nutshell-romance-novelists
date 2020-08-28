f57d8b5472df467d2269c8f3ebd2c7ed2f667f88
ardisrtilson <email@example.com>
2020-08-28 13:00:57 -0500

    Added user differentiation for newsentries. Added display of both your and friends' journal entries.

--- src/scripts/home/HomeHTMLGenerator.js
+++ src/scripts/home/HomeHTMLGenerator.js
@@ -1,4 +1,5 @@
-export const NewsHTMLConverter = (newsObject) => {
+export const NewsHTMLConverter = (type, newsObject) => {
+    if (type === "yours"){
     return `
         <section class="newsCard--${newsObject.id}">
         <div class="news--title">Title: ${ newsObject.title }</div>
@@ -9,4 +10,19 @@ export const NewsHTMLConverter = (newsObject) => {
         <button id="editNews--${newsObject.id}">Edit Article</button>
         </section>
             `
+}
+if (type === "theirs"){
+    return `
+        <article class="theirs">
+        <section class="newsCard--${newsObject.id}">
+        <div class="news--title">Title: ${ newsObject.title }</div>
+        <div class="news--synopsis">Synopsis: ${ newsObject.synopsis }</div>
+        <div class="news--URL"><a href="${ newsObject.url }">View Article</a></div>
+        <div class="news--date">Date: ${ newsObject.date }</div>
+        <button id="deleteNews--${newsObject.id}">Delete Article</button>
+        <button id="editNews--${newsObject.id}">Edit Article</button>
+        </section>
+        <article>
+            `
+}
 }
--- src/scripts/home/HomeList.js
+++ src/scripts/home/HomeList.js
@@ -1,6 +1,7 @@
 import { getNews, useNews, deleteNews} from "./HomeProvider.js"
 import {getWeatherData } from "../weather/WeatherProvider.js"
 import { NewsHTMLConverter } from "./HomeHTMLGenerator.js"
+import { getUserFriends, useUserFriends, getUsers, useUsers} from "../users/usersDataProvider.js"
 
 const contentTarget = document.querySelector(".newsContainer")
 const eventHub = document.querySelector(".container")
@@ -17,20 +18,75 @@ eventHub.addEventListener("click", clickEvent => {
 })
 
 export const NewsList = () => {
+    let currentUserId = parseInt(sessionStorage.getItem("activeUser"))
     getWeatherData()
     .then(() => {
     getNews()
+    .then(() => {
+    getUserFriends()
         .then(() => {
-            const allNews = useNews()
-            render(allNews)
+        const allNews = useNews()
+        const allFriends = useUserFriends()
+        const userFriends = allFriends.filter(f => {
+            if(currentUserId === f.friendId || currentUserId === f.userId) {
+                return f
+            }
         })
+        let currentRelationships = userFriends.filter(friend => {
+            if (currentUserId === friend.userId || currentUserId === friend.friendId) {
+                return friend
+            }
+        })
+
+        const foundNews = currentRelationships.map(relationship => {
+            return allNews.find(news => {
+                if (news.userId === relationship.friendId || news.userId === relationship.userId){
+                    if (news.userId != currentUserId){
+                    return news
+                    }
+                }
+            })
+        })
+        const friendNews = foundNews.filter(stories => {
+            if (stories != undefined){
+                return stories
+            }
+        })
+
+        const userNews = allNews.filter(stories => {
+            if (stories.userId === currentUserId) {
+                return stories
+            }
+        })
+        render(friendNews, userNews)
+        })
+
+    })
     })
     }     
 
-    const render = (newsArray) => {
+    const render = (friendNews, userNews) => {
         let itemHTML = ""
-        newsArray.map(item => {
-        itemHTML += NewsHTMLConverter(item)
+    userNews.map(item => {
+        itemHTML += NewsHTMLConverter("yours", item)
+    })
+    friendNews.map(item => {
+        itemHTML += NewsHTMLConverter("theirs", item)
     })
         contentTarget.innerHTML = itemHTML
     }
+
+    // 
+    // 
+
+
+    // const filteredNews = userFriends.map(f => {
+    //     console.log(f.userId)
+    //     return allNews.find(story => {
+    //         if(story.userId === f.userId) {
+    //             return story
+    //         }
+    //     })
+    // })
+    // console.log(filteredNews)
+    // render(filteredNews)
